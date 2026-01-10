85e767e73b8e24dee38ef44b6be9ffb5fda0d0e4
Radha Goel <167902290+RadhaGoel@users.noreply.github.com>
2026-01-11 00:38:41 +0530

    Time: 5 ms (65.22%) | Memory: 47.1 MB (56.23%) - LeetSync

---
+++ 72-edit-distance/edit-distance.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int minDistance(String s1, String s2) {
+        int m = s1.length();
+        int n = s2.length();
+        int dp[][] = new int[m+1][n+1];
+
+        //BASE CASE
+        for(int i = 1; i < m+1; i++){
+            dp[i][0] = i;//delete all charactrs from s1
+        }
+        for(int j = 1; j < n+1; j++){
+            dp[0][j] = j;//delete all from s2
+        }
+
+        //filling dp
+        for(int i = 1; i < m+1; i++){
+            for(int j = 1; j < n+1; j++){
+                if(s1.charAt(i-1) == s2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                    else {
+                    dp[i][j] = 1 + Math.min(
+                        dp[i - 1][j],                // delete
+                        Math.min(
+                            dp[i][j - 1],            // insert
+                            dp[i - 1][j - 1]          // replace
+                        )
+                    );
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
