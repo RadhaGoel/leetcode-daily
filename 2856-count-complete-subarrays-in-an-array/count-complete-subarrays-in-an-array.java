938b67746c992923faaace2a74420e6ce78d2e14
Radha Goel <167902290+RadhaGoel@users.noreply.github.com>
2026-02-05 00:25:03 +0530

    Time: 7 ms (94.76%) | Memory: 47.2 MB (48.47%) - LeetSync

--- 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.java
+++ 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.java
@@ -1,27 +1,24 @@
-import java.util.HashMap;
-import java.util.HashSet;
-
 class Solution {
 
     public int countCompleteSubarrays(int[] nums) {
         int n = nums.length;
 
-        // 1️⃣ Count total distinct elements
+        //Count total distinct elements
         int totalDistinct = countDistinctElements(nums);
 
         HashMap<Integer, Integer> map = new HashMap<>();
         int left = 0;
         int ans = 0;
 
-        // 2️⃣ Sliding window
+        //Sliding window
         for (int right = 0; right < n; right++) {
             map.put(nums[right], map.getOrDefault(nums[right], 0) + 1);
 
-            // 3️⃣ When window is complete
+            //When window is complete
             while (map.size() == totalDistinct) {
                 ans += (n - right);
 
-                // shrink window
+                //Shrink window
                 map.put(nums[left], map.get(nums[left]) - 1);
                 if (map.get(nums[left]) == 0) {
                     map.remove(nums[left]);
