e77c13445476ea2a4e9a9a628052037c2dc0dc3c
Radha Goel <167902290+RadhaGoel@users.noreply.github.com>
2026-04-16 21:09:22 +0530

    Time: 97 ms (74.75%) | Memory: 192 MB (27.27%) - LeetSync

--- 3750-closest-equal-element-queries/closest-equal-element-queries.java
+++ 3750-closest-equal-element-queries/closest-equal-element-queries.java
@@ -2,34 +2,33 @@ class Solution {
 
     public List<Integer> solveQueries(int[] nums, int[] queries) {
         int n = nums.length;
-        HashMap<Integer, ArrayList<Integer>> numsPos = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            numsPos.computeIfAbsent(nums[i], k -> new ArrayList<>()).add(i);
-        }
-        for (ArrayList<Integer> pos : numsPos.values()) {
-            int x = pos.get(0);
-            int last = pos.get(pos.size() - 1);
-            pos.add(0, last - n);
-            pos.add(x + n);
-        }
-        List<Integer> result = new ArrayList<>();
-        for (int q : queries) {
-            int x = nums[q];
-            ArrayList<Integer> posList = numsPos.get(x);
+        int[] left = new int[n];
+        int[] right = new int[n];
 
-            if (posList.size() == 3) {
-                result.add(-1);
-                continue;
+        HashMap<Integer, Integer> pos = new HashMap<>();
+        for (int i = -n; i < n; i++) {
+            if (i >= 0) {
+                left[i] = pos.getOrDefault(nums[i], i - n);
             }
+            pos.put(nums[((i % n) + n) % n], i);
+        }
 
-            int idx = Collections.binarySearch(posList, q);
-            if (idx < 0) idx = -idx - 1;
+        pos.clear();
+        for (int i = 2 * n - 1; i >= 0; i--) {
+            if (i < n) {
+                right[i] = pos.getOrDefault(nums[i], i + n);
+            }
+            pos.put(nums[i % n], i);
+        }
 
-            int dist = Math.min(
-                posList.get(idx + 1) - posList.get(idx),
-                posList.get(idx) - posList.get(idx - 1)
-            );
-            result.add(dist);
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < queries.length; i++) {
+            int x = queries[i];
+            if (x - left[x] == n) {
+                result.add(-1);
+            } else {
+                result.add(Math.min(x - left[x], right[x] - x));
+            }
         }
         return result;
     }
