fd7fb6d3102f9b2e44a2d422e8367ce274b913d0
Radha Goel <167902290+RadhaGoel@users.noreply.github.com>
2026-04-28 22:45:19 +0530

    Time: 99 ms (49.62%) | Memory: 118.1 MB (6.87%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.java
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.java
@@ -1,32 +1,50 @@
 class Solution {
 
     public int minOperations(int[][] grid, int x) {
-        // Create a list to store all the numbers from the grid
-        ArrayList<Integer> numsArray = new ArrayList<>();
+        List<Integer> numsArray = new ArrayList<>();
         int result = 0;
 
-        // Flatten the grid into numsArray
+        // Flatten the grid into numsArray and check remainder condition
         for (int row = 0; row < grid.length; row++) {
             for (int col = 0; col < grid[0].length; col++) {
+                if (grid[row][col] % x != grid[0][0] % x) return -1;
+                // If any element has a different remainder than the first
+                // element, it is impossible to make all elements equal, so
+                // return -1
                 numsArray.add(grid[row][col]);
             }
         }
 
-        // Sort numsArray in non-decreasing order to easily find the median
         Collections.sort(numsArray);
 
         int length = numsArray.size();
-        // Store the median element as the final common value
-        int finalCommonNumber = numsArray.get(length / 2);
+        int prefixIndex = 0;
+        int suffixIndex = length - 1;
 
-        // Iterate through each number in numsArray
-        for (int number : numsArray) {
-            // If the remainder when divided by x is different, return -1
-            if (number % x != finalCommonNumber % x) {
-                return -1;
+        // Move prefixIndex and suffixIndex towards the middle
+        while (prefixIndex < suffixIndex) {
+            // If the prefix of equal elements is shorter than the suffix
+            if (prefixIndex < length - suffixIndex - 1) {
+                // Calculate the number of operations required to extend the prefix
+                int prefixOperations =
+                    ((prefixIndex + 1) *
+                        (numsArray.get(prefixIndex + 1) -
+                            numsArray.get(prefixIndex))) /
+                    x;
+                result += prefixOperations;
+                // Move the prefix index forward
+                prefixIndex++;
+            } else {
+                // Calculate the number of operations required to extend the suffix
+                int suffixOperations =
+                    ((length - suffixIndex) *
+                        (numsArray.get(suffixIndex) -
+                            numsArray.get(suffixIndex - 1))) /
+                    x;
+                result += suffixOperations;
+                // Move the suffix index backward
+                suffixIndex--;
             }
-            // Add the number of operations required to make the current number equal to finalCommonNumber
-            result += Math.abs(finalCommonNumber - number) / x;
         }
 
         return result;
