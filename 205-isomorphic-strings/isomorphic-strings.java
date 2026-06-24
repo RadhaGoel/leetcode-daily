4a21f130b8990e27a779a7595e6acf1dfcd12381
Radha Goel <167902290+RadhaGoel@users.noreply.github.com>
2026-06-24 14:10:43 +0530

    Time: 6 ms (88.94%) | Memory: 43.7 MB (85.87%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        int [] indexS = new int [200]; //200 ascii values
+        int [] indexT = new int [200];
+
+        int len = s.length();
+
+        for(int i = 0; i < len; i++){
+            if(indexS[s.charAt(i)] != indexT[t.charAt(i)]){
+                return false;
+            }
+            indexS[s.charAt(i)] = i + 1;
+            indexT[t.charAt(i)] = i + 1;
+        }
+        return true;
+    }
+}
