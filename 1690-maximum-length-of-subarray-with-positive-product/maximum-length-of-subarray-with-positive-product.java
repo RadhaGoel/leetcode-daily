cc5e231f13c4857133db45bdd7aa11e682da4182
Radha Goel <167902290+RadhaGoel@users.noreply.github.com>
2026-07-09 10:59:38 +0530

    Time: 3 ms (94.28%) | Memory: 76.8 MB (63.64%) - LeetSync

--- 1690-maximum-length-of-subarray-with-positive-product/maximum-length-of-subarray-with-positive-product.java
+++ 1690-maximum-length-of-subarray-with-positive-product/maximum-length-of-subarray-with-positive-product.java
@@ -1,8 +1,8 @@
 class Solution {
     public int getMaxLen(int[] nums) {
-        int pos = 0;
-        int neg = 0;
         int ans = 0;
+        int neg = 0;
+        int pos = 0;
 
         for(int num : nums){
             if(num > 0){
@@ -13,12 +13,11 @@ class Solution {
                 int oldPos = pos;
                 int oldNeg = neg;
 
-                pos = (oldNeg == 0) ? 0 : oldNeg +1;
+                pos = (oldNeg == 0) ? 0 : oldNeg+1;
                 neg = oldPos + 1;
             }
-
             else{
-                pos = 0; 
+                pos = 0;
                 neg = 0;
             }
             ans = Math.max(ans, pos);
